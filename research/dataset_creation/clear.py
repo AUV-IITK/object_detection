dc32a804a994cb0d310ab997293ea2d8d121af87
Ayush Gupta <7ayushgupta@gmail.com>
2019-05-18 01:04:46 +0000

    Deleted the make files:
    Fix the class id of vampire to 0
    
    Added 700 iterations weight
    
    Made change to saving:
    
    renamed file to 700

--- research/dataset_creation/clear.py
+++ research/dataset_creation/clear.py
@@ -1,25 +1,19 @@
 import glob, os
 from random import shuffle
 
-current_dir = "/home/ayush/Projects/object_detection_ws/src/research/dataset_creation/Images/"
+current_dir = "/home/ayush/Projects/object_detection_ws/src/research/darknet_alexab/vampire/Images"
 txt_glob = glob.iglob(os.path.join(current_dir, "*.txt"))
-jpg_glob = glob.iglob(os.path.join(current_dir, "*.jpg"))
 txt_titles = []
-jpg_titles = []
 
 for pathAndFilename in txt_glob:
-    title = os.path.splitext(pathAndFilename)[0]
-    txt_titles.append(title)
-    print("File title: {}.txt".format(title))
-
-for pathAndFilename in jpg_glob:
-    title = os.path.splitext(pathAndFilename)[0]
-    jpg_titles.append(title)
-    print("File title: {}.jpg".format(title))
-
-for name in jpg_titles:
-	print("here")
-	if name not in txt_titles:
-		os.remove(str(name)+".jpg") 
-		print("removing")
+	print(pathAndFilename)
+	f = open(pathAndFilename, "r")
+	contents = f.read()
+	contents_string = str(contents)
+	contents_string = contents_string.split(" ", 1)[1]
+	contents_string = "0 " + contents_string
+	print(contents_string)
+	f.close()
+	f = open(pathAndFilename, "w+")
+	f.write(contents_string)
 
